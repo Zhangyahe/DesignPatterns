2cefaaa8549aa43108d37ad04fb85b269111019f
雅核 <59279330+Zhangyahe@users.noreply.github.com>
2020-04-29 21:43:19 +0800

    Single04-使用静态内部类的方式1、实现懒加载2、实现线程安全

---
+++ src/SingleMode/Single04.java
@@ -0,0 +1,25 @@
+package SingleMode;
+
+/**
+ * @description: 推荐的完美写法1-使用静态内部类的方式
+ *  1、解决什么时候用什么时候加载的问题
+ *  2、解决线程安全的问题
+ * @author: YaHe
+ * @createDate: 2020/4/29
+ * @version: 1.0
+ */
+public class Single04 {
+    private Single04(){
+    }
+    private static class Single04Holder{
+        private static final Single04 INSTANCE = new Single04();
+    }
+    public static Single04 getInstance(){
+        return Single04Holder.INSTANCE;
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> System.out.println(Single03.getInstance().hashCode())).start();
+        }
+    }
+}
